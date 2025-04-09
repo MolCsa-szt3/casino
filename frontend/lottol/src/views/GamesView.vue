ef1e3ee4c2c37a764ed131bc52a039d1547e2024
szt3molcsa <szt3molcsa@vasvari.org>
2025-04-09 10:05:15 +0200

    eh close enough

--- frontend/lottol/src/views/GamesView.vue
+++ frontend/lottol/src/views/GamesView.vue
@@ -1,3 +1,28 @@
-<template></template>
+<script setup>
+import { useLotteryStore } from "@/stores/lottery";
+const lotteryStore = useLotteryStore();
+</script>
+
+<template>
+  <h1 class="text-center">Fogadások</h1>
+  <div class="container border">
+    <h5
+      v-if="lotteryStore.selectedLotteryList.length == 0"
+      class="text-danger text-center"
+    >
+      <b>Még nincs leadott fogadás.</b>
+    </h5>
+    <div v-else>
+      <ul>
+        <li v-for="selected in lotteryStore.selectedLotteryList">
+          {{ selected }}
+        </li>
+      </ul>
+      <button class="btn btn-danger" @click="lotteryStore.emptyLotteryList()">
+        Lista törlése
+      </button>
+    </div>
+  </div>
+</template>
 
 <style></style>
--- frontend/lottol/src/views/HomeView.vue
+++ frontend/lottol/src/views/HomeView.vue
@@ -1,5 +1,34 @@
-<script setup></script>
+<script setup>
+import { useLotteryStore } from "@/stores/lottery";
+const lotteryStore = useLotteryStore();
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <h1 class="text-center">Lottók</h1>
+    <div class="container">
+      <div class="row">
+        <div
+          v-for="lotto in lotteryStore.types"
+          class="card col-md-6 col-lg-4 col-xl-3"
+        >
+          <div class="card-body">
+            <h4 class="card-title">{{ lotto }}</h4>
+            <img
+              class="card-img-top"
+              :src="lotteryStore.getPic(lotto)"
+              alt="Card image"
+            />
+            <p class="card-text">TEsting makes me feel good</p>
+            <button
+              @click="lotteryStore.selectLottery(lotto)"
+              class="btn btn-success"
+            >
+              Játtszom
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
