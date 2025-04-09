e9e8dfb9ebd5d2857eb2fd63668cf001beb14cf4
szt3molcsa <szt3molcsa@vasvari.org>
2025-04-09 10:43:08 +0200

    console done, desktop start

---
+++ SzenerceVallalat.Console/Models/Users.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzenerceVallalat.Console.Models
+{
+    public class User
+    {
+        private string _email;
+        private string _name;
+        private decimal _money;
+        private int _id;
+        
+
+        public User(string name, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new InvalidDataException("Név nem lehet üres!");
+            if (!email.Contains('@')) throw new InvalidDataException("Helytelen az Email formátuma!");
+            _name = name;
+            _email = email;
+            _money = 0;
+        }
+        public bool HasMoney()
+        {
+            return _money != 0;
+        }
+        public void AddMoney(decimal amount)
+        {
+            if (amount <= 0) throw new ArgumentOutOfRangeException("Csak pozitív mennyiségű pénzt lehet hozzáadni!");
+            _money += amount;
+        }
+
+        public string Email { get => _email; set => _email = value; }
+        public string Name { get => _name; set => _name = value; }
+        public decimal Money { get => _money; set => _money = value; }
+        public int Id { get => _id; set => _id = value; }
+
+        public void Bet(decimal amount)
+        {
+            if (amount <= 0) throw new ArgumentOutOfRangeException("Csak pozitív mennyiségű pénzt lehet fogadni!");
+            if (_money < amount) throw new ArgumentException("Nincs elég pénze a felhaználónak!");
+            _money -= amount;
+        }
+        public override string ToString()
+        {
+            return $"{_name} ({_email}) {_money:c0}";
+        }
+    }
+}
---
+++ SzenerceVallalat.Console/Repos/UserRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SzenerceVallalat.Console.Models;
+using SzenerceVallalat.Console.DbModels;
+
+namespace SzenerceVallalat.Console.Repos
+{
+    public class UserRepo
+    {
+        private PlayersContext _context;
+        
+        public UserRepo() 
+        {
+            _context = new PlayersContext();
+        }
+        public List<User> GetAllUsers() 
+        {
+            return _context.Players.ToList(); 
+        }
+        public List<User> GetUsersAboveAmount(decimal minMoney) 
+        { 
+            return _context.Players.Where(x => x.Money > minMoney).ToList();
+        }
+        public decimal GetTotalMoneyUsersHave()
+        {
+            return _context.Players.Sum(x => x.Money);
+        }
+    }
+}
